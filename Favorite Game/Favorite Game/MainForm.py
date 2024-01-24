dbe6aeb2344d1136ccdf081712664d1a7a04e0a0
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-01-24 14:35:11 -0600

    FavGame
    FavGame SharpDevelop

---
+++ Favorite Game/Favorite Game/MainForm.py
@@ -0,0 +1,102 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._label1 = System.Windows.Forms.Label()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._label2 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.Black
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.Yellow
+		self._button1.Location = System.Drawing.Point(102, 260)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(163, 104)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Reveal"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.Black
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.ForeColor = System.Drawing.Color.Yellow
+		self._label1.Location = System.Drawing.Point(269, 40)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(307, 151)
+		self._label1.TabIndex = 1
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.Black
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.Color.Yellow
+		self._button2.Location = System.Drawing.Point(368, 260)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(163, 104)
+		self._button2.TabIndex = 2
+		self._button2.Text = "Hide"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.Black
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.Color.Yellow
+		self._button3.Location = System.Drawing.Point(653, 260)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(163, 104)
+		self._button3.TabIndex = 3
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.Black
+		self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.ForeColor = System.Drawing.Color.Yellow
+		self._label2.Location = System.Drawing.Point(2, 40)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(261, 151)
+		self._label2.TabIndex = 4
+		self._label2.Text = "My Favorite Game Is:"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.DarkRed
+		self.ClientSize = System.Drawing.Size(874, 400)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Favorite Game"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		self._label1.Text = "Red Dead Redemption II. However, Cyberpunk 2077 is a close second"
+
+	def Button2Click(self, sender, e):
+		self._label1.Text = ""
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
