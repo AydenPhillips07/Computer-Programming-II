8d1ad79025ec5baf017d5d810f154dc61cc23c10
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-01-30 20:34:33 +0000

    finished LP4-9. 1/30/24

--- LP4-9.py
+++ LP4-9.py
@@ -1,2 +1,11 @@
 import random
-random.randit(1, 20)
+cn = random.randint(1, 20)
+pn = int(input("Enter a number between 1 and 20!:  "))
+if cn == pn:
+  print("Computer's Number: ", cn)
+  print("Your Number: ", pn)
+  print("You Won!")
+else:
+  print("Computer's Number: ", cn)
+  print("Your Number: ", pn)
+  print("Better Luck Next Time!")
