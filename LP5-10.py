ab3cd119f60f1ed49479654dc228dd193f9abc29
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-02-02 20:35:57 +0000

    w

--- LP5-10.py
+++ LP5-10.py
@@ -1,7 +1,12 @@
 import math
 num1 = int(input("Enter a Number: "))
 num2 = int(input("Enter a Second Number: "))
-ans = math.gcd(num1, num2) 
-print("The GCD is: ", ans)
+#ans = math.gcd(num1, num2) easy way
+#print("The GCD is: ", ans)
+while (num1 > 0 and num2 > 0):
+  temp = num1 % num2
+  num1 = num2
+  num2 = temp
 
+print("The GCD is: ", num1)
   
