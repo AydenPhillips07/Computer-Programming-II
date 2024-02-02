98a3a4328126a6b7766b6bf65e3f78dced48a157
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-02-02 20:16:03 +0000

    finished LP5-10  2/2/24

--- LP5-10.py
+++ LP5-10.py
@@ -1,2 +1,7 @@
-num1 = int(input("Enter a Number"))
-num2 = int(input("Enter a Second Number"))
+import math
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Second Number: "))
+ans = math.gcd(num1, num2) 
+print("The GCD is: ", ans)
+
+  
