6457303f43465bc431c3cbd67cc35cbf8f34fa0d
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-02-14 14:37:57 -0600

    Update 2/14/23

---
+++ pg272/pg272/MainForm.py
@@ -0,0 +1,155 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._radioButton1 = System.Windows.Forms.RadioButton()
+		self._radioButton2 = System.Windows.Forms.RadioButton()
+		self._radioButton3 = System.Windows.Forms.RadioButton()
+		self._label1 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
+		self._label6 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# radioButton1
+		# 
+		self._radioButton1.BackColor = System.Drawing.Color.Crimson
+		self._radioButton1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._radioButton1.Location = System.Drawing.Point(12, 78)
+		self._radioButton1.Name = "radioButton1"
+		self._radioButton1.Size = System.Drawing.Size(294, 76)
+		self._radioButton1.TabIndex = 0
+		self._radioButton1.TabStop = True
+		self._radioButton1.Text = "DayTime (6:00 a.m. through 5:59 P.M.)"
+		self._radioButton1.UseVisualStyleBackColor = False
+		# 
+		# radioButton2
+		# 
+		self._radioButton2.BackColor = System.Drawing.Color.Crimson
+		self._radioButton2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._radioButton2.Location = System.Drawing.Point(12, 160)
+		self._radioButton2.Name = "radioButton2"
+		self._radioButton2.Size = System.Drawing.Size(294, 76)
+		self._radioButton2.TabIndex = 1
+		self._radioButton2.TabStop = True
+		self._radioButton2.Text = "Evening (6:00 p.m. through 11:59 P.M.)"
+		self._radioButton2.UseVisualStyleBackColor = False
+		# 
+		# radioButton3
+		# 
+		self._radioButton3.BackColor = System.Drawing.Color.Crimson
+		self._radioButton3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._radioButton3.Location = System.Drawing.Point(12, 242)
+		self._radioButton3.Name = "radioButton3"
+		self._radioButton3.Size = System.Drawing.Size(294, 76)
+		self._radioButton3.TabIndex = 2
+		self._radioButton3.TabStop = True
+		self._radioButton3.Text = "Off-Peak (12:00 a.m. through 5:59 A.M.)"
+		self._radioButton3.UseVisualStyleBackColor = False
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.Crimson
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(537, 26)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(174, 76)
+		self._label1.TabIndex = 3
+		self._label1.Text = "Call Time (In Minutes)"
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# textBox1
+		# 
+		self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.Location = System.Drawing.Point(537, 110)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(174, 44)
+		self._textBox1.TabIndex = 4
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.Crimson
+		self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(342, 78)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(150, 76)
+		self._label2.TabIndex = 5
+		self._label2.Text = "$0.07"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.Crimson
+		self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(342, 160)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(150, 76)
+		self._label3.TabIndex = 6
+		self._label3.Text = "$0.12"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.Color.Crimson
+		self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.Location = System.Drawing.Point(342, 242)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(150, 76)
+		self._label4.TabIndex = 7
+		self._label4.Text = "$0.05"
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label5
+		# 
+		self._label5.BackColor = System.Drawing.Color.Crimson
+		self._label5.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label5.Location = System.Drawing.Point(12, 4)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(294, 61)
+		self._label5.TabIndex = 8
+		self._label5.Text = "Time of Day Call Took Place"
+		self._label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label6
+		# 
+		self._label6.BackColor = System.Drawing.Color.Crimson
+		self._label6.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label6.Location = System.Drawing.Point(361, 4)
+		self._label6.Name = "label6"
+		self._label6.Size = System.Drawing.Size(131, 61)
+		self._label6.TabIndex = 9
+		self._label6.Text = "$0.07"
+		self._label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		self._label6.Click += self.Label6Click
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(814, 491)
+		self.Controls.Add(self._label6)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._radioButton3)
+		self.Controls.Add(self._radioButton2)
+		self.Controls.Add(self._radioButton1)
+		self.Name = "MainForm"
+		self.Text = "pg272"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Label6Click(self, sender, e):
+		pass
