70466f2b5360b1ddb0e115f66c62f70cd91166ea
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-02-20 20:18:59 +0000

    update 2/20/24

---
+++ prog54cfuncs.py
@@ -0,0 +1,19 @@
+pi = 3.14159
+rad = float(input("Enter Radius :"))
+
+def circ(rad, pi):
+  c = 2 * rad * pi
+  
+
+def area(rad, pi):
+  are = pi + rad**2
+  
+  
+
+
+def main():
+  pi = 3.14159
+  rad = float(input("Enter Radius :"))
+
+
+  
