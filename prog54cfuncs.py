c622d1afa59657b8db8348f33697d0b2a59e4454
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-02-21 20:15:17 +0000

    update 2/21/24

--- prog54cfuncs.py
+++ prog54cfuncs.py
@@ -1,8 +1,8 @@
 def calcArea(pi, radius):
-  return pi * radius**2
+  return round(pi * radius**2, 2)
 
 def calcCirc(pi, radius):
-  return 2 * pi * radius
+  return round(2 * pi * radius, 2)
 
 def areaCirc(pi, radius):
   area = calcArea(pi, radius)
@@ -11,8 +11,10 @@ def areaCirc(pi, radius):
 
 def main():
   pi = 3.1459
-  radius = float(input("Enter Radius :"))
-  
+  radius = float(input("Enter Radius: "))
+  a, c = areaCirc(pi, radius)
+  print(f"Area: {a}\nCircumference: {c}")
+  pass
 
 
 if __name__ == "__main__":
