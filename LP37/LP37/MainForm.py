ef1b47ea045b811437ca8b506a25e5f0c6af16e0
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-02-26 14:30:10 -0600

    update 2/26/24
    LP3-7 lesson.

---
+++ LP37/LP37/MainForm.py
@@ -0,0 +1,117 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._label1 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.OrangeRed
+		self._label1.Font = System.Drawing.Font("MS Gothic", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(59, 113)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(554, 143)
+		self._label1.TabIndex = 0
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# textBox1
+		# 
+		self._textBox1.Location = System.Drawing.Point(59, 12)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(554, 38)
+		self._textBox1.TabIndex = 1
+		# 
+		# textBox2
+		# 
+		self._textBox2.Location = System.Drawing.Point(59, 72)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(554, 38)
+		self._textBox2.TabIndex = 2
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("MS Gothic", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(59, 269)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(172, 75)
+		self._button1.TabIndex = 3
+		self._button1.Text = "Exit"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Font = System.Drawing.Font("MS Gothic", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(237, 269)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(173, 75)
+		self._button2.TabIndex = 4
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.Font = System.Drawing.Font("MS Gothic", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(416, 269)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(197, 75)
+		self._button3.TabIndex = 5
+		self._button3.Text = "Calculate"
+		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Black
+		self.ClientSize = System.Drawing.Size(654, 356)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label1)
+		self.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self.Name = "MainForm"
+        self.Text = "LP37"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button3Click(self, sender, e):
+		num1 = int(self._textBox1.Text)
+		num2 = int(self._textBox2.Text)
+		from clLP37 import *
+		obj1 = ClLP37(num1, num2)
+		obj2 = ClLP37(num1, num2)
+		obj1.calc()
+		obj2.calc()
+		div1 = obj1.getDiv()
+		mod1 = obj1.getMod()
+		div2, mod2 = obj2.getDivMod()
+		self._label1.Text = str(num1) + "/" + str(num2) + "=" + str(div1) + "\n"
+		self._label1.Text += str(num1) + "%" + str(num2) + "=" + str(mod1) + "\n"
+		self._label1.Text += str(num2) + "/" + str(num1) + "=" + str(div2) + "\n"
+		self._label1.Text += str(num2) + "%" + str(num1) + "=" + str(mod2) + "\n"
+
+	def Button2Click(self, sender, e):
+		self._label1.Text = ""
+		self._textBox1.Text = ""
+		self._textBox2.Text = ""
+
+	def Button1Click(self, sender, e):
+		Application.Exit()
+		
