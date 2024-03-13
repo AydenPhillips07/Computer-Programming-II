74f97e390aa3bb0b4ab860a29aa916ba09f6c484
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-03-13 19:38:27 +0000

    update 3/13/24

---
+++ Cl213f.py
@@ -0,0 +1,16 @@
+class Cl213f:
+  def __init__(self, kwh):
+    self.kwh = kwh
+    self.cost = 0.0
+
+  def calc(self):
+    if self.kwh <= 2000:
+      self.cost = 0.07
+    elif self.kwh > 2000 and self.kwh <= 10000:
+      self.cost = 0.05
+    elif self.kwh > 10000:
+      self.cost = 0.04
+    
+
+  def __str__(self):
+    return f"The cost of {self.kwh} is", self.kwh * self.cost
