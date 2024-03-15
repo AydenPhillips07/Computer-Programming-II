575374305acf788a85477dfc78796d91d0c6652a
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-03-15 19:34:01 +0000

    Update 3/15/24

--- prog54cClass.py
+++ prog54cClass.py
@@ -1,15 +1,20 @@
 class Circle:
-  def __init__(self, rad, pi):
-    pi = 3.1459
-    rad = int(input("Enter Radius: "))
-
-  def getArea(self, rad, pi):
-
-
-  def getCircum(self, rad, pi):
-  
-  
-  def calculate(self, rad, pi):
-
-
+  def __init__(self, radius):
+    self.radius = radius
     
+
+
+  def calc(self):
+    pass
+
+  
+   
+  
+  
+  
+  
+  
+
+  
+    
+  
