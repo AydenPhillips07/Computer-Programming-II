de9595975e289aff898a67e9d3b40aee5a7ca461
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2024-03-26 19:32:38 +0000

    Update 3/26/24
    prog701g.py finished

--- Prog213b.py
+++ Prog213b.py
@@ -3,18 +3,18 @@
 #200	-	299	$5.40
 #300 	&	 up	$5.15
 
-#Quant = int(input("Enter Quantity: "))
+Quant = int(input("Enter Quantity: "))
 def main():
-Price = 0.00
-Amount = 0.00
-if Quant >= 0 and Quant <= 99:
-  Price = 5.95
-elif Quant >= 100 and Quant <= 199:
-  Price = 5.75
-elif Quant >= 200 and Quant <= 299:
-  Price = 5.40
-elif Quant >= 300:
-  Price = 5.15
+  Price = 0.00
+  Amount = 0.00
+  if Quant >= 0 and Quant <= 99:
+    Price = 5.95
+  elif Quant >= 100 and Quant <= 199:
+    Price = 5.75
+  elif Quant >= 200 and Quant <= 299:
+    Price = 5.40
+  elif Quant >= 300:
+    Price = 5.15
 
-Amount = Quant * Price
-print("Quantity:", Quant, "Price per item:", Price, "Total Amount:", Amount)
+  Amount = Quant * Price
+  print("Quantity:", Quant, "Price per item:", Price, "Total Amount:", Amount)
